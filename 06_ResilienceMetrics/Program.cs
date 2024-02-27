7a3aae0d045119c5aee4af8c253d5a3daaaacdc7
giorgio.lasala <giorgio.lasala@avanade.com>
2024-02-27 20:01:54 +0100

    wip otel

--- 06_ResilienceMetrics/Program.cs
+++ 06_ResilienceMetrics/Program.cs
@@ -6,17 +6,31 @@ using Microsoft.Extensions.Logging;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using Polly;
+using Polly.Retry;
 using Polly.Simmy;
+using Polly.Telemetry;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 IServiceCollection services = builder.Services;
 
-services.AddLogging(builder => builder.ClearProviders().AddInMemory());
+services.AddLogging(/*builder => builder.ClearProviders().AddInMemory()*/);
 services.AddSingleton<StatsService>();
 services.AddScoped<LayoutUI>();
 var httpClientBuilder = services.AddHttpClient<MealDbClient>();
 
-httpClientBuilder.AddStandardResilienceHandler();
+var telemetryOptions = new TelemetryOptions
+{
+    // Configure logging
+    LoggerFactory = LoggerFactory.Create(builder => builder.AddConsole())
+};
+telemetryOptions.MeteringEnrichers.Add(new CustomMeteringEnricher());
+
+httpClientBuilder.AddResilienceHandler("standard", builder =>
+{
+    builder.AddTimeout(TimeSpan.FromSeconds(1));
+    builder.ConfigureTelemetry(telemetryOptions);
+}
+);
 
 httpClientBuilder.AddResilienceHandler("chaos", (ResiliencePipelineBuilder<HttpResponseMessage> builder) =>
 {
@@ -31,26 +45,48 @@ httpClientBuilder.AddResilienceHandler("chaos", (ResiliencePipelineBuilder<HttpR
 
 services.AddOpenTelemetry().WithMetrics(opts => opts
                 .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("ResilienceMetrics"))
-                .AddMeter("Polly")
-                .AddConsoleExporter());
-                //.AddOtlpExporter(options =>
-                //{
-                //    options.Endpoint = new Uri(builder.Configuration.GetValue<string>("OtlpEndpointUri")
-                //                               ?? throw new InvalidOperationException());
-                //}));
+                .AddMeter("*")
+                .AddConsoleExporter()
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri("http://localhost:4318/v1/metrics");
+                }));
 
 var host = builder.Build();
 
 var service = host.Services.GetRequiredService<MealDbClient>();
-var layoutUI = host.Services.GetRequiredService<LayoutUI>();
+//var layoutUI = host.Services.GetRequiredService<LayoutUI>();
 var statsService = host.Services.GetRequiredService<StatsService>();
 using var cancellationSource = new CancellationTokenSource();
 var cancellationToken = cancellationSource.Token;
 
 while (true)
 {
-    layoutUI.UpdateUI();
+    //layoutUI.UpdateUI();
     Thread.Sleep(1000);
     statsService.TotalRequests++;
     var response = await service.GetRandomMealAsync(cancellationToken);
+}
+
+
+internal class MyTelemetryListener : TelemetryListener
+{
+    public override void Write<TResult, TArgs>(in TelemetryEventArguments<TResult, TArgs> args)
+    {
+        Console.WriteLine($"*****Telemetry event occurred: {args.Event.EventName}");
+    }
+}
+
+internal sealed class CustomMeteringEnricher : MeteringEnricher
+{
+    public override void Enrich<TResult, TArgs>(in EnrichmentContext<TResult, TArgs> context)
+    {
+        // You can read additional details from any resilience event and use it to enrich the telemetry
+        if (context.TelemetryEvent.Arguments is OnRetryArguments<TResult> retryArgs)
+        {
+            // See https://github.com/open-telemetry/semantic-conventions/blob/main/docs/general/metrics.md for more details
+            // on how to name the tags.
+            context.Tags.Add(new("retry.attempt", retryArgs.AttemptNumber));
+        }
+    }
 }
