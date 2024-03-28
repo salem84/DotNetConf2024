a877a72de3843dbeb4e252e80d85d2e0a062718c
giorgio.lasala <giorgio.lasala@avanade.com>
2024-03-28 19:03:03 +0100

    refit project

---
+++ 06_ResilienceRefit/CustomMealDbRefitClient.cs
@@ -0,0 +1,46 @@
+﻿namespace DotNetConf2024.ResilienceRefit;
+
+using DotNetConf2024.Common;
+using Refit;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+public class CustomMealDbRefitClient
+{
+    private readonly IMealDbRefitClient _client;
+    private readonly StatsService _statsService;
+    private readonly IMeterFactory _meterFactory;
+
+    public CustomMealDbRefitClient(IMealDbRefitClient client, StatsService statsService, IMeterFactory meterFactory)
+    {
+        _client = client;
+        _statsService = statsService;
+        _meterFactory = meterFactory;
+    }
+    public async Task<SearchMealResponse> GetRandomMealAsync()
+    {
+        _statsService.TotalRequests++;
+
+        var watch = Stopwatch.StartNew();
+        try
+        {
+            var mealResult = await _client.GetRandomMealAsync();
+            string mealName = mealResult.Meals.FirstOrDefault()?.Name ?? string.Empty;
+            _statsService.AddHttpResultEvent(new HttpResultEvent(DateTime.Now, 200, watch.ElapsedMilliseconds, mealName));
+            return mealResult;
+        }
+        catch (Exception ex)
+        {
+            int statusCode = ex is ApiException apiEx ? (int)apiEx.StatusCode : -1;
+            _statsService.AddHttpResultEvent(new HttpResultEvent(DateTime.Now, statusCode, watch.ElapsedMilliseconds, ex.Message));
+            return new SearchMealResponse([]);
+        }
+    }
+}
+
+
+public interface IMealDbRefitClient
+{
+    [Get("/random.php")]
+    Task<SearchMealResponse> GetRandomMealAsync();
+}
---
+++ 06_ResilienceRefit/Program.cs
@@ -0,0 +1,49 @@
+﻿using DotNetConf2024.Common;
+using DotNetConf2024.ResilienceRefit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+using Polly.Simmy;
+using Refit;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+IServiceCollection services = builder.Services;
+
+services.AddLogging(builder => builder.ConfigureAppLogging());
+services.AddScoped<LayoutUI>();
+
+var httpClientBuilder = services
+    .AddRefitClient<IMealDbRefitClient>()
+    .ConfigureHttpClient(client =>
+    {
+        client.BaseAddress = new Uri("https://www.themealdb.com/api/json/v1/1/");
+    });
+
+
+httpClientBuilder.AddStandardResilienceHandler();
+
+services.AddScoped<CustomMealDbRefitClient>();
+
+httpClientBuilder.AddResilienceHandler("chaos", (ResiliencePipelineBuilder<HttpResponseMessage> builder) =>
+{
+    // Set the chaos injection rate to 30%
+    const double InjectionRate = 0.3;
+
+    _ = builder
+        .AddChaosLatency(InjectionRate, TimeSpan.FromSeconds(1)) // Add latency to simulate network delays
+        .AddChaosOutcome(InjectionRate, () => new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError)); // Simulate server errors
+});
+
+using IHost host = builder.Build();
+
+using IServiceScope scope = host.Services.CreateScope();
+
+var service = scope.ServiceProvider.GetService<CustomMealDbRefitClient>();
+var layoutUI = scope.ServiceProvider.GetRequiredService<LayoutUI>();
+
+while (true)
+{
+    layoutUI.UpdateUI();
+    var response = await service.GetRandomMealAsync();
+    Thread.Sleep(1000);
+}
